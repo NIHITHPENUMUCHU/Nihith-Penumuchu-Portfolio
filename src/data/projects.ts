e9aa466957897be089934a38d1747690a6ddcd10
Nihith Penumuchu <135858540+NIHITHPENUMUCHU@users.noreply.github.com>
2024-12-05 14:43:13 +0530

    projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,7 +4,7 @@ export const projects: Project[] = [
   {
     title: "3D Car Racing",
     description: "A small 3D car Racing done done using JavaScript, To operate use arrows",
-    image: "https://png.pngtree.com/background/20230527/original/pngtree-neon-racing-cars-on-dark-road-picture-image_2761009.jpg",
+    image: "/3d car racing.png",
     tags: ["JavaScript"],
     github: "https://github.com/NIHITHPENUMUCHU/3D-Racing-Game",
     live: "https://3d-racing-game.vercel.app/"
@@ -12,7 +12,7 @@ export const projects: Project[] = [
   {
     title: "Portfolio Website",
     description: "A modern portfolio website showcasing my work and skills",
-    image: "https://images.unsplash.com/photo-1547658719-da2b51169166?auto=format&fit=crop&q=80",
+    image: "/SkilBridge1.png",
     tags: ["React", "Tailwind", "TypeScript", "Framer Motion"],
     github: "https://github.com/NIHITHPENUMUCHU/Portfolio",
     live: "https://nihithportfolio-nwdqj0ck8-ra2111026010124s-projects.vercel.app"
@@ -20,7 +20,7 @@ export const projects: Project[] = [
   {
     title: "E-commerce Platform",
     description: "A feature-rich e-commerce platform with user authentication and payment integration",
-    image: "https://images.unsplash.com/photo-1661956602116-aa6865609028?auto=format&fit=crop&q=80",
+    image: "public/feastfinder1.png",
     tags: ["React", "Node.js", "MongoDB", "Stripe"],
     github: "https://github.com/NIHITHPENUMUCHU/Ecommerce-Platform",
     live: "https://ecommerce-platform-nihith.vercel.app"
